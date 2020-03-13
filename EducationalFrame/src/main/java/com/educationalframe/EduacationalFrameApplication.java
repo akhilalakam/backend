a1ff6b8d4abddfd1989265f1decc3424193004de
akhilalakam <tech.akhil24@gmail.com>
2020-03-14 00:57:49 +0530

    initial commit

---
+++ EducationalFrame/src/main/java/com/educationalframe/EduacationalFrameApplication.java
@@ -0,0 +1,13 @@
+package com.educationalframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduacationalFrameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduacationalFrameApplication.class, args);
+	}
+
+}
