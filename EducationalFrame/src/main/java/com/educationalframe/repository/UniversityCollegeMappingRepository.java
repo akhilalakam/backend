0e1497e428a26d70b89ce8d1b8b88780b6e52c51
sudha <your_email@example.com>
2020-03-14 21:46:33 +0530

    added services

--- EducationalFrame/src/main/java/com/educationalframe/repository/UniversityCollegeMappingRepository.java
+++ EducationalFrame/src/main/java/com/educationalframe/repository/UniversityCollegeMappingRepository.java
@@ -1,10 +1,14 @@
 package com.educationalframe.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.educationalframe.entity.UniversityCollegeMapping;
 
 public interface UniversityCollegeMappingRepository extends JpaRepository<UniversityCollegeMapping, Long>{
 
+	
+	public List<UniversityCollegeMapping> findByUniversityUniversityId(Long universityId);
 }
 	
---
+++ EducationalFrame/src/main/java/com/educationalframe/response/UniversityOfCollegeResponse.java
@@ -0,0 +1,29 @@
+package com.educationalframe.response;
+
+public class UniversityOfCollegeResponse {
+
+	
+	private Long collegeId;
+	private String collegeName;
+	private String collegeLocation;
+	public Long getCollegeId() {
+		return collegeId;
+	}
+	public void setCollegeId(Long collegeId) {
+		this.collegeId = collegeId;
+	}
+	public String getCollegeName() {
+		return collegeName;
+	}
+	public void setCollegeName(String collegeName) {
+		this.collegeName = collegeName;
+	}
+	public String getCollegeLocation() {
+		return collegeLocation;
+	}
+	public void setCollegeLocation(String collegeLocation) {
+		this.collegeLocation = collegeLocation;
+	}
+	
+	
+}
