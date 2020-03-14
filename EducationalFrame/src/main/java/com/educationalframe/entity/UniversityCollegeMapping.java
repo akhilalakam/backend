102e63e08759be36e926949e13e93ce6dcdff8e1
balaforjava <pbcmmr1994@gmail.com>
2020-03-14 15:44:07 +0530

    added setters and getters

--- EducationalFrame/src/main/java/com/educationalframe/entity/UniversityCollegeMapping.java
+++ EducationalFrame/src/main/java/com/educationalframe/entity/UniversityCollegeMapping.java
@@ -1,7 +1,25 @@
 package com.educationalframe.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class UniversityCollegeMapping {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long universityCollegeMappingId;
+
+	public Long getUniversityCollegeMappingId() {
+		return universityCollegeMappingId;
+	}
+
+	public void setUniversityCollegeMappingId(Long universityCollegeMappingId) {
+		this.universityCollegeMappingId = universityCollegeMappingId;
+	}
+	
+	
 	
 }
--- EducationalFrame/src/main/java/com/educationalframe/entity/UserRoleMapping.java
+++ EducationalFrame/src/main/java/com/educationalframe/entity/UserRoleMapping.java
@@ -1,5 +1,7 @@
 package com.educationalframe.entity;
 
 public class UserRoleMapping {
+	
+	
 
 }
